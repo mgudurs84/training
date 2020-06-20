d5be12967f1ad0a9dcf9da221272c6daea07ab8a
mgudurs84 <murali.gudur84@gmail.com>
2020-06-20 03:16:02 -0400

    first

--- src/index.js
+++ src/index.js
@@ -3,11 +3,20 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import NavBar from './components/NavBar';
+import { ThemeProvider } from '@material-ui/styles';
+import { theme, themeWithRtl } from './theme';
+import Summary from './views/CustomerManagementDetails/Summary/index'
+import Logs from './views/CustomerManagementDetails/Logs'
+import Statistics from './views/Statistics'
+import './mock'
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <ThemeProvider theme={theme}>
+    <Statistics />
+    <Summary />
+    <Logs />
+  </ThemeProvider>,
   document.getElementById('root')
 );
 
